0e89dd1abbaabdbb1057d641a48613d4d071cd05
David Shadow <92687730+David-Shadow@users.noreply.github.com>
2022-02-18 23:44:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ from os import environ as env
 API_ID = int(env.get('API_ID', ''))
 API_HASH = env.get('API_HASH', '')
 BOT_TOKEN = env.get('BOT_TOKEN', '')
+ADMINS = env.get('ADMINS', 1794161348)
+ADMINS = [int(ADMINS) if (' ' not in ADMINS) else int(admin) for admin in str(ADMINS).split(' ')]
