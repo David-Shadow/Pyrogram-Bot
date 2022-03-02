7e093097baa9ef7c0da0e4ab5e986e5627ae355b
David Shadow <92687730+David-Shadow@users.noreply.github.com>
2022-03-02 18:46:10 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -14,8 +14,7 @@ from pyrogram.raw.all import layer
 from config import (
     API_ID,
     API_HASH,
-    BOT_TOKEN,
-    LOG_CHAT
+    BOT_TOKEN
 )
 plugins = dict(root="bot/plugins")
 
@@ -36,29 +35,10 @@ class Bot(Client):
         me = await self.get_me()
         self.username = '@' + me.username
         logging.info(f"{me.first_name} started with Pyrogram v{__version__} (Layer {layer}) started on @{me.username}.")
-        try:
-            startmsg = await self.send_message(
-                           chat_id=LOG_CHAT,
-                           text='**Bot started🥳.**',
-                           parse_mode = 'md'
-                       )
-            await startmsg.pin()
-        except Exception as e:
-            logging.exception(f"Error in sending message to log chat: {e}")
-            pass
 
     async def stop(self, *args):
-        logging.info("Bot stopped. Bye.")
-        try:
-            msg = await self.send_message(
-                           chat_id=LOG_CHAT,
-                           text='**Bot stopped!.**',
-                           parse_mode = 'md'
-                       )
-            await msg.pin()
-        except Exception as e:
-            logging.exception(f"Error in sending message to log chat: {e}")
         await super().stop()
+        logging.info("Bot stopped. Bye.")
 
 
 app = Bot()
